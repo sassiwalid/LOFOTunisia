f82afb456c23f0e128a865223410ccdda2e83d9d
sassiwalid <sassi.walid@outlook.com>
2020-05-18 00:37:02 +0200

    add remote config framework

---
+++ RemoteConfig/RemoteConfig/RemoteConfig.h
@@ -0,0 +1,19 @@
+//
+//  RemoteConfig.h
+//  RemoteConfig
+//
+//  Created by walid sassi on 5/18/20.
+//  Copyright © 2020 walid sassi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RemoteConfig.
+FOUNDATION_EXPORT double RemoteConfigVersionNumber;
+
+//! Project version string for RemoteConfig.
+FOUNDATION_EXPORT const unsigned char RemoteConfigVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteConfig/PublicHeader.h>
+
+
